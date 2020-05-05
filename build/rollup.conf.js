037a3d75f6be0a52da1782f964914898ab91bf3d
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-05-05 12:39:44 +0530

    fix: update rollup config

--- build/rollup.conf.js
+++ build/rollup.conf.js
@@ -2,39 +2,41 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import vue from 'rollup-plugin-vue';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
+export default [
+  // ESM build to be used with webpack/rollup.
+  {
+    input: 'src/index.js',
+    output: {
+      format: 'esm',
+      name: 'VPip',
+      file: 'dist/v-pip.esm.js',
+    },
+    plugins: [babel(), vue()],
+    external: ['@vue/composition-api'],
+  },
+  // CommonJS build
+  {
+    input: 'src/index.js',
+    output: {
+      format: 'cjs',
+      name: 'VPip',
+      file: 'dist/v-pip.cjs.js',
+    },
+    plugins: [babel(), commonjs(), vue()],
+    external: ['@vue/composition-api'],
+  },
+  // UMD build.
+  {
+    input: 'src/index.js',
+    output: {
       format: 'umd',
       name: 'VPip',
-      file: 'dist/v-pip.js',
+      file: 'dist/v-pip.umd.js',
       globals: {
-        vue: 'vue',
+        '@vue/composition-api': 'vueCompositionApi',
       },
     },
-    {
-      format: 'cjs',
-      file: 'dist/v-pip.cjs.js',
-      globals: {
-        vue: 'vue',
-      },
-    },
-    {
-      format: 'es',
-      file: 'dist/v-pip.esm.js',
-      globals: {
-        vue: 'vue',
-      },
-    },
-  ],
-  plugins: [
-    commonjs(),
-    vue(),
-    babel({
-      exclude: 'node_modules/**',
-      runtimeHelpers: true,
-    }),
-  ],
-  external: ['vue'],
-};
+    plugins: [vue()],
+    external: ['@vue/composition-api'],
+  },
+];
