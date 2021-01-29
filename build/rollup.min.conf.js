ac0fdcab00bce250e396d9ece3453e3d04d1728c
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-01-29 16:41:14 +0530

    feat: add `commonjs()` as rollup plugin in config

--- build/rollup.min.conf.js
+++ build/rollup.min.conf.js
@@ -1,5 +1,6 @@
 import alias from '@rollup/plugin-alias';
 import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
@@ -16,6 +17,7 @@ export default {
   },
   plugins: [
     resolve({ extensions, browser: true }),
+    commonjs({ extensions, exclude: 'src/**' }),
     alias({
       entries: {
         vue: 'vue/dist/vue.runtime.esm-browser.prod.js',
