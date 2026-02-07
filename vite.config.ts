a76589adef48d6f8f9ed96646ab13270992ddd32
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-02-08 04:12:01 +0530

    refactor: update vite.config.ts for ESM-only output

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,21 @@
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import pkg from './package.json';
-
-const banner = `/*!
-* ${pkg.name} v${pkg.version}
-* (c) ${new Date().getFullYear()} ${pkg.author.name}
-* @license ${pkg.license}
-*/`;
 
 export default defineConfig({
   plugins: [vue()],
   build: {
     target: 'esnext',
-    minify: 'esbuild',
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VPip',
-      formats: ['es', 'cjs', 'umd'],
-      fileName: pkg.name,
-    },
-    commonjsOptions: {
-      extensions: ['.js', '.ts', '.vue'],
-      exclude: 'src/**',
-      include: 'node_modules/**',
+      formats: ['es'],
+      fileName: 'v-pip',
     },
     rollupOptions: {
       external: ['vue'],
       output: {
         exports: 'named',
-        banner,
-        strict: true,
-        globals: {
-          vue: 'vue',
-        },
         assetFileNames: (assetInfo) => {
           if (assetInfo.name === 'style.css') return 'v-pip.css';
           return assetInfo.name;
